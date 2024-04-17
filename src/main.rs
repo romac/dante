fa9c89306f7ae73d4085be9e3da98cb84dbe4884
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-04-17 21:46:02 +0200

    Parse DeviceTree information

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,45 @@ pub unsafe extern "C" fn kmain(hart_id: usize, phys_dtb: usize) -> ! {
     debug_println!("  hart: {hart_id}");
     debug_println!("  dtb (physical): 0x{phys_dtb:x}");
 
+    debug_dtb();
+
     loop {
         wfi();
+    }
+}
+
+static MY_FDT: &[u8] = include_bytes!("../virt.dtb");
+
+fn debug_dtb() {
+    let fdt = fdt::Fdt::new(MY_FDT).unwrap();
+
+    debug_println!("\nDeviceTree:");
+    debug_println!("  Model:               {}", fdt.root().model());
+    debug_println!("  Compatible with:     {}", fdt.root().compatible().first());
+    debug_println!("  CPUs:                {}", fdt.cpus().count());
+    debug_println!(
+        "  First memory region: {:#X}",
+        fdt.memory().regions().next().unwrap().starting_address as usize
+    );
+
+    let chosen = fdt.chosen();
+    if let Some(bootargs) = chosen.bootargs() {
+        debug_println!("  Boot arguments:      {:?}", bootargs);
+    }
+
+    if let Some(stdout) = chosen.stdout() {
+        debug_println!("  Stdout device:       {}", stdout.name);
+    }
+
+    let soc = fdt.find_node("/soc");
+    debug_println!(
+        "  Has /soc node?       {}",
+        if soc.is_some() { "yes" } else { "no" }
+    );
+    if let Some(soc) = soc {
+        debug_println!("  Children:");
+        for child in soc.children() {
+            debug_println!("    {}", child.name);
+        }
     }
 }
