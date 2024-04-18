f3012239bc66b7d5738992f1db73b87bc1c46ccd
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-04-19 00:44:40 +0200

    Add prelude and test

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod boot;
 mod console;
 mod dtb;
 mod memory;
+mod prelude;
 mod sbi;
 
 use core::arch::global_asm;
@@ -17,6 +18,8 @@ use core::panic::PanicInfo;
 
 use boot::BootInfo;
 use memory::PhysAddr;
+
+use prelude::*;
 
 pub const BANNER: &str = r#"
       ___           ___           ___           ___           ___     
@@ -62,12 +65,46 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     debug_println!("  DeviceTree:");
     debug_println!("    Physical: {}", boot_info.dtb_addr);
     debug_println!("    Virtual:  {}", boot_info.dtb_addr.to_virt());
+
+    let memory_region = boot_info.memory_region();
+
     debug_println!("  Memory region:");
-    debug_println!("    {}", boot_info.memory_region());
+    debug_println!("    Start: {}", memory_region.start);
+    debug_println!("    End:   {}", memory_region.end());
+    debug_println!("    Size:  {} bytes", memory_region.size);
 
     // dtb::debug_dtb(&boot_info.fdt);
 
     allocator::init_heap(boot_info);
+
+    let heap_value = Box::new(41);
+    debug_println!("heap_value at {:p}", heap_value);
+    debug_println!("heap_value: {}", *heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    debug_println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    debug_println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    debug_println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
+    let string = String::from("crash");
+    debug_println!("string at {:p}", string.as_str());
+
+    debug_println!("It did not {string}!");
 
     sbi::sbi_shutdown()
 }
---
+++ src/prelude.rs
@@ -0,0 +1,4 @@
+pub use alloc::boxed::Box;
+pub use alloc::rc::Rc;
+pub use alloc::string::String;
+pub use alloc::{vec, vec::Vec};
